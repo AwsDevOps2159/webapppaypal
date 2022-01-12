cc9af3ece96917b73d6e0b66daf9a9195b62ab93
moulali <shaiksameer2159@gmail.com>
2022-01-12 12:01:20 +0530

    b

---
+++ src/test/java/com/paypal/SampleTest.java
@@ -0,0 +1,21 @@
+package com.paypal;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Sample.
+ */
+public class SampleTest
+ 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
